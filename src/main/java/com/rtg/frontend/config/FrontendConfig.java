33ba62d7f8c51db87db18938cbe1a9685e03713c
TheoHappy <teodore473@gmail.com>
2021-12-11 22:26:28 +0200

    mend
    init commit

--- src/main/java/com/rtg/frontend/config/FrontendConfig.java
+++ src/main/java/com/rtg/frontend/config/FrontendConfig.java
@@ -1,17 +1,17 @@
-//package com.rtg.frontend.config;
-//
-//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.client.RestTemplate;
-//
-//@Configuration
-//class FrontendConfig {
-//
-//    @Bean
+package com.rtg.frontend.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+class FrontendConfig {
+
+    @Bean
 //    @LoadBalanced
-//    public RestTemplate restTemplate() {
-//        return new RestTemplate();
-//    }
-//
-//}
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
